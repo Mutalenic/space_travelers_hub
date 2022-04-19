988e652dc9e1369163c756a291fa3490ea7a9639
Chia Carlyle <chiacarlyle@gmail.com>
2022-04-19 13:06:25 +0100

    Add a placeholder table, which will be changed when data is fetched from the api

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,9 +1,33 @@
+import './Missions.css';
+
 const Missions = () => (
 
-  <>
-    <h1>Missions</h1>
-    <p>Missions appear here!</p>
-  </>
+  <div className="mission-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>Thaicom</th>
+          <td>
+            iuahawuiduaghduahduiawdid udauuu
+            iwidasdiudisdiads thuisdbis dummy text to occupy the space
+            . you dont ahve to understand whats writen here but just pretend
+            that you do.
+
+          </td>
+          <td>
+            <button type="button">Join Mission</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 );
 
 export default Missions;
