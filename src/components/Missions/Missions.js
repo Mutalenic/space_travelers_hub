84a9520a98ad1d57ca7a09f1b48ef22cd7c07369
Chia Carlyle <chiacarlyle@gmail.com>
2022-04-20 11:39:54 +0100

    Implement functionality for the join and leave buttons

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,8 +1,10 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import './Missions.css';
+import { joinMission, leaveMission } from '../../redux/Missions/Missions';
 
 const Missions = () => {
   const allMissions = useSelector((state) => state.missionsReducer.missions);
+  const dispatch = useDispatch();
   return (
 
     <div className="mission-table">
@@ -20,8 +22,12 @@ const Missions = () => {
             <tr key={mission.id}>
               <td><b>{mission.name}</b></td>
               <td>{mission.description}</td>
-              <td><button type="button" className="member-btn">Not a Member</button></td>
-              <td><button type="button" className="join-btn">Join Mission</button></td>
+              <td><button type="button" className="member-btn">NOT A MEMBER</button></td>
+              {mission.reserved
+                ? <td><button type="button" className="leave-btn" onClick={() => dispatch(leaveMission(mission.id))}>Leave Mission</button></td>
+                : <td><button type="button" className="join-btn" onClick={() => dispatch(joinMission(mission.id))}>Join Mission</button></td>}
+              {' '}
+
             </tr>
           ))}
         </tbody>
