ba501f32291969f058844b424669a71f20c0830e
Chia Carlyle <chiacarlyle@gmail.com>
2022-04-20 05:18:43 +0100

    Provide basic styling for the header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,31 @@
+/* eslint-disable react/no-unescaped-entities */
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
+import logo from '../../img/logo.png';
 
 const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <NavLink to="/">Rockets</NavLink>
-      </li>
-      <li>
-        <NavLink to="missions">Missions</NavLink>
-      </li>
-      <li>
-        <NavLink to="profile">Profile</NavLink>
-      </li>
-      <li>
-        <NavLink to="dragons">Dragons</NavLink>
-      </li>
-    </ul>
-  </nav>
+  <header>
+    <div className="left">
+      <img className="logo" alt="logo" src={logo} />
+      <h1>Space Travelers' Hub</h1>
+    </div>
+    <nav>
+      <ul className="nav">
+        <li>
+          <NavLink to="/">Rockets</NavLink>
+        </li>
+        <li>
+          <NavLink to="missions">Missions</NavLink>
+        </li>
+        <li>
+          <NavLink to="profile">Profile</NavLink>
+        </li>
+        <li>
+          <NavLink to="dragons">Dragons</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default Navbar;
