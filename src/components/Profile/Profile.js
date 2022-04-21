3039d9b9f9621c29f2b6b0133621a91146a3006d
Nicholas Mutale <nicomutale@gmail.com>
2022-04-21 17:22:32 +0200

    display reserved rockets in profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,12 +5,16 @@ const Profile = () => {
   const missionsJoined = useSelector(
     (state) => state.missionsReducer.missions.filter((mission) => mission.reserved === true),
   );
+  const bookRockets = useSelector(
+    (state) => state.rocketsReducer.rockets.filter((rocket) => rocket.reserved === true),
+  );
   return (
     <div>
       <div className="profileContainer">
-        <h2 className="leftprof-heading">My Missions</h2>
-        <table className="leftTable">
-          {
+        <div className="missionContainer">
+          <h2 className="leftprof-heading">My Missions</h2>
+          <table className="leftTable">
+            {
         missionsJoined.length
           ? missionsJoined.map((mission) => (
 
@@ -21,7 +25,25 @@ const Profile = () => {
           ))
           : <p>You have not joined any mission yet.</p>
        }
-        </table>
+
+          </table>
+        </div>
+        <div className="rocketContainer">
+          <h2 className="rightprof-heading">My Rockets</h2>
+          <table className="rightTable">
+            {
+        bookRockets.length
+          ? bookRockets.map((rocket) => (
+
+            <td key={rocket.id}>
+              <p>{rocket.name}</p>
+            </td>
+
+          ))
+          : <p>You have not reserved any rocket yet.</p>
+       }
+          </table>
+        </div>
       </div>
     </div>
   );
